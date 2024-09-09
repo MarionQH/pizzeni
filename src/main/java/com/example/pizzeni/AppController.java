a806a83af78ea123016ff6b9ad757f4505e3e82e
Marion <marion.huet49@gmail.com>
2024-09-09 16:30:05 +0200

    base html et css

---
+++ src/main/java/com/example/pizzeni/AppController.java
@@ -0,0 +1,15 @@
+package com.example.pizzeni;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("home")
+    public String index() {
+        return "base.html";
+    }
+
+}
--- src/test/java/lalala.java
+++
@@ -1,5 +0,0 @@
-public class lalala {
-
-
-    //ju
-}
