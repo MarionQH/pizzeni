c732cf9a353e236ae3d5e2e3ebabe6b281916363
Marion <marion.huet49@gmail.com>
2024-09-09 15:52:17 +0200

    initialisation du projet

---
+++ src/main/java/com/example/pizzeni/PizzeniApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizzeni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizzeni/PizzeniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizzeni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
