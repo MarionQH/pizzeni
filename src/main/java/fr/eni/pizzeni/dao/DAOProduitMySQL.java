bbf5354e62f24ec07e5f94e994fa7c96c8bb97d9
Marion <marion.huet49@gmail.com>
2024-09-10 14:08:59 +0200

    SQL dao produit sauf save

--- src/main/java/fr/eni/pizzeni/dao/DAOProduitMySQL.java
+++ src/main/java/fr/eni/pizzeni/dao/DAOProduitMySQL.java
@@ -1,9 +1,11 @@
 package fr.eni.pizzeni.dao;
 
 import fr.eni.pizzeni.bo.Produit;
+import fr.eni.pizzeni.bo.TypeProduit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -35,26 +37,59 @@ public class DAOProduitMySQL implements IDAOProduit {
             produit.setPrix(rs.getLong("prix"));
             produit.setImageUrl(rs.getString("image_url"));
 
-//            TypeProduit typeProduit = new TypeProduit();
-//            typeProduit.setTitre(rs.getString("libelle"));
-//            typeProduit.setId(rs.getLong("id_type_produit"));
+            TypeProduit typeProduit = new TypeProduit();
+            typeProduit.setLibelle(rs.getString("libelle"));
+            typeProduit.setId(rs.getLong("id_type_produit"));
+
+            return produit;
         }
-    }
+    };
 
 
 
     @Override
     public List<Produit> selectProduits() {
-        return List.of();
+        return jdbcTemplate.query("SELECT p.id_produit, p.nom, p.description, p.prix, p.image_url, tp.id_type_produit as id_type_produit, tp.libelle FROM produit p JOIN type_produit tp ON p.type_produit_id_type_produit = tp.id_type_produit", PRODUIT_ROW_MAPPER);
     }
 
     @Override
     public Produit selectProduitById(Long id) {
-        return null;
+        List<Produit> produits = jdbcTemplate.query("SELECT p.id_produit, p.nom, p.description, p.prix, p.image_url, tp.id_type_produit as id_type_produit, tp.libelle FROM produit p JOIN type_produit tp ON p.type_produit_id_type_produit = p.id_produit WHERE p.id_produit=?", PRODUIT_ROW_MAPPER, id);
+        if (produits.size() == 0) {
+            return null;
+        }
+        //0 correspond à l'index de la liste (premier élément)
+        return produits.get(0);
     }
 
     @Override
     public void saveProduit(Produit produit) {
+//
+//        // 1. Insérer le film dans la table childrenmovie
+//        String sql = "INSERT INTO childrenmovie (title,year,duration,synopsis,url,id_genre) VALUES (:titleMovie,:yearMovie,:durationMovie,:synopsisMovie,:urlMovie,:genreMovie)";
+//        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+//        mapSqlParameterSource.addValue("titleMovie", movie.title);
+//        mapSqlParameterSource.addValue("yearMovie", movie.year);
+//        mapSqlParameterSource.addValue("durationMovie", movie.duration);
+//        mapSqlParameterSource.addValue("synopsisMovie", movie.synopsis);
+//        mapSqlParameterSource.addValue("urlMovie", movie.url);
+//        mapSqlParameterSource.addValue("genreMovie", movie.getGenre().getId());
+//
+//        // Exécuter la requête pour insérer le film
+//        namedParameterJdbcTemplate.update(sql, mapSqlParameterSource);
+//
+//        // 2. Récupérer l'id du film récemment inséré (si auto-incrémenté par la base de données)
+//        Long childrenMovieId = namedParameterJdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", new MapSqlParameterSource(), Long.class);
+//
+//        // 3. Insérer la liste des participants (acteurs) dans la table acteurs
+//        String sqlActeur = "INSERT INTO acteurs (id_film, id_participant) VALUES (:idMovie, :idParticipant)";
+//        for (Participant participant : movie.getParticipants()) {
+//            MapSqlParameterSource mapParticipantSource = new MapSqlParameterSource();
+//            mapParticipantSource.addValue("idMovie",childrenMovieId);
+//            mapParticipantSource.addValue("idParticipant", participant.getId()); // Utiliser l'ID du participant
+//
+//            namedParameterJdbcTemplate.update(sqlActeur, mapParticipantSource);
+//        }
 
     }
 }
