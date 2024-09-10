bd8768c9d09a3a58539ca2f55d594719491e419e
Marion <marion.huet49@gmail.com>
2024-09-10 09:28:20 +0200

    controller login

---
+++ src/main/java/fr/eni/pizzeni/AuthController.java
@@ -0,0 +1,14 @@
+package fr.eni.pizzeni;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("login")
+        public String login() {
+        return "login.html";
+        }
+
+}
