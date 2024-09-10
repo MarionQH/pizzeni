710f056d5f9b31e3786e2afb078c10da0a939f2d
Chloe <chloe.gn29@gmail.com>
2024-09-10 10:08:15 +0200

    controllers

--- src/main/java/fr/eni/pizzeni/AppController.java
+++ src/main/java/fr/eni/pizzeni/AppController.java
@@ -3,13 +3,49 @@ package fr.eni.pizzeni;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 
-    @GetMapping("home")
+    @GetMapping("accueil")
     public String index() {
-        return "base.html";
+        return "accueil.html";
     }
 
+
+    @GetMapping("ajout-pizza")
+    public String getAjoutPizza() {
+        return "ajout-pizza.html";
+    }
+
+    @PostMapping("ajout-pizza")
+    public String postAjoutPizza() {
+        return "redirect:/ajout-pizza";
+    }
+
+
+    @GetMapping("creation-commande")
+    public String getCreationCommande() {
+        return "creation-commande.html";
+    }
+
+    @PostMapping("creation-commande")
+    public String postCreationCommande() {
+        return  "redirect:/creation-commande";
+    }
+
+    @GetMapping("carte")
+    public String getCarte() {
+        return "carte.html";
+    }
+
+    @GetMapping("liste-commandes")
+        public String getListCommandes() {
+            return "commandes.html";
+        }
+
+
+
 }
+
--- src/main/java/fr/eni/pizzeni/AuthController.java
+++ src/main/java/fr/eni/pizzeni/AuthController.java
@@ -2,13 +2,30 @@ package fr.eni.pizzeni;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
 
     @GetMapping("login")
-        public String login() {
-        return "login.html";
+        public String getLogin() {
+        return "connexion.html";
         }
 
-}
+
+   @PostMapping("login")
+    public String postLogin() {
+        return "redirect:/login";
+   }
+
+   @GetMapping ("logout")
+        public String getLogout() {
+            return "connexion.html";
+       }
+
+   @PostMapping ("logout")
+    public String postLogout() {
+        return "redirect:/login";
+   }
+   }
+
