3a54942425d481d3fd1faa6f7f1dcdf3e16b77d2
Chloe <chloe.gn29@gmail.com>
2024-09-10 11:28:19 +0200

    Added Param Bdd in app properties and code in controller to test connection

--- src/main/java/fr/eni/pizzeni/AppController.java
+++ src/main/java/fr/eni/pizzeni/AppController.java
@@ -1,12 +1,42 @@
 package fr.eni.pizzeni;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.lang.reflect.Member;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Controller
 public class AppController {
+
+// test connexion bdd
+//    @Autowired
+//    JdbcTemplate jdbcTemplate;
+//
+//    static final RowMapper<Member> MEMBER_ROW_MAPPER = new RowMapper<Member>() {
+//
+//        @Override
+//        public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+//            Member user = new Member();
+//
+//            user.setFirstName(rs.getString("prenom"));
+//            user.setLastName(rs.getString("nom"));
+//            user.setEmail(rs.getString("email"));
+//            user.setId(rs.getLong("id"));
+//
+//
+//
+//            return user;
+//        }
+//
+//    };
+
+
+
 
     @GetMapping("")
     public String getBase() {
