cc4d7924d6a747640c6ec395aed81dc8b867abdc
Chloe <chloe.gn29@gmail.com>
2024-09-11 15:34:01 +0200

    changes in controller

--- src/main/java/fr/eni/pizzeni/ihm/AppController.java
+++ src/main/java/fr/eni/pizzeni/ihm/AppController.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -44,13 +47,13 @@ public class AppController {
     }
 
 
-    @GetMapping("ajout-pizza")
-    public String getAjoutPizza(Long id, Model model) {
+    @GetMapping("ajout-pizza/{id}")
+
+    public String getAjoutPizza(@PathVariable(required = false) Long id, Model model) {
 
         Produit produit = new Produit();
         List<TypeProduit> typesProduits = typeProduitManager.getTypesProduits();
         System.out.println(produit);
-
 
 
         System.out.println(typesProduits);
@@ -136,11 +139,11 @@ public class AppController {
         return "redirect:/carte";
     }
 
-    @GetMapping("modifier-produit")
-    public String getModifierProduit(Long id) {
-
+    @GetMapping("modifier-produit/**")
+    public String getModifierProduit(@RequestParam Long id) {
        //todo: s'assurer que la page ajout pizza est chargée avec le bon produit
-        return "ajout-pizza.html";
+
+        return "redirect:/ajout-pizza/{id}";
     }
 }
 
