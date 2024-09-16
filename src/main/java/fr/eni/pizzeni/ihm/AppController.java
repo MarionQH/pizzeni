c43fd321c4d56286d81c76e7f7e9dc0ce430d296
Marion <marion.huet49@gmail.com>
2024-09-16 14:02:19 +0200

    commit dans le vent

--- src/main/java/fr/eni/pizzeni/ihm/AppController.java
+++ src/main/java/fr/eni/pizzeni/ihm/AppController.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class AppController {
 
     private final ProduitManager produitManager;
+
     @Autowired
     private DetailCommandeManager detailCommandeManager;
 
