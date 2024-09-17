88ba14449d495b65ea48969c071160d7f3ab9bc9
Chloe <chloe.gn29@gmail.com>
2024-09-17 16:57:42 +0200

    i dunno

--- src/main/java/fr/eni/pizzeni/bll/CommandeManager.java
+++ src/main/java/fr/eni/pizzeni/bll/CommandeManager.java
@@ -1,8 +1,10 @@
 package fr.eni.pizzeni.bll;
 
 import fr.eni.pizzeni.bo.Commande;
+import fr.eni.pizzeni.bo.DetailCommande;
 import fr.eni.pizzeni.dao.IDAOCommande;
 import fr.eni.pizzeni.dao.IDAODetailCommande;
+import fr.eni.pizzeni.dao.IDAOProduit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,9 @@ public class CommandeManager implements ICommandeManager {
     IDAODetailCommande detailCommande;
     @Autowired
     IDAOCommande daoCommande;
+
+    @Autowired
+    IDAOProduit daoProduit;
 
     @Override
     public List<Commande> getCommandes() {
@@ -29,6 +34,17 @@ public class CommandeManager implements ICommandeManager {
 
         Commande commande = daoCommande.selectCommandeById(id);
         commande.setDetailsCommandes(detailCommande.selectDetailsCommandeByIdCommande(id));
+        List<DetailCommande> detailsCommandes = commande.getDetailsCommandes();
+
+        //Todo récupérer les produits dans les détails produits
+//        for (int i = 0 ; i < detailsCommandes.size() ; i++) {
+//
+//          detailsCommandes(i).setProduit(daoProduit.selectProduitById(detailsCommandes(i).getId()));
+//
+//        }
+
+
+
 
         return commande;
     }
